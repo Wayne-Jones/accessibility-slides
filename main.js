6f1d4dd5605fd234d4cee85fedc8c3afd1482fe6
Wayne Jones <wej916@gmail.com>
2023-03-21 15:25:08 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import './styles/style.css'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/white.css'
+import 'reveal.js/plugin/highlight/monokai.css'
+
+import './styles/nt.css'
+import Reveal from 'reveal.js'
+import Highlight from 'reveal.js/plugin/highlight/highlight'
+import MarkDown from 'reveal.js/plugin/markdown/markdown'
+import Notes from 'reveal.js/plugin/notes/notes'
+import Zoom from 'reveal.js/plugin/zoom/zoom'
+
+const deck = new Reveal()
+deck.initialize({ hash: true, plugins: [ MarkDown, Highlight, Notes, Zoom ] })
